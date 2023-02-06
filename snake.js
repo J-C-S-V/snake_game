c14e9dd8f5164d8263d37093798dea6ae17c78ac
Juan Sanchez <juansan1993@hotmail.com>
2023-02-05 23:10:25 -0500

    Styling game

--- snake.js
+++ snake.js
@@ -1,25 +1,21 @@
-const cvs = document.getElementById("snake");
-const ctx = cvs.getContext("2d");
-
+const cvs = document.getElementById('snake');
+const ctx = cvs.getContext('2d');
 // Create unit
 const box = 32;
-
 // Load images
-
 const ground = new Image();
-ground.src = "img/ground.png";
+ground.src = 'img/ground.png';
 
 const foodImg = new Image();
-foodImg.src = "img/Juank-snake.jpg";
+foodImg.src = 'img/Juank-snake.jpg';
 
 const canvicha = new Image();
 
-canvicha.id = "joder";
+canvicha.id = 'joder';
 
 console.log(canvicha);
 
 // Load audio files
-
 const dead = new Audio();
 const eat = new Audio();
 const up = new Audio();
@@ -27,15 +23,14 @@ const left = new Audio();
 const right = new Audio();
 const down = new Audio();
 
-dead.src = "audio/dead.mp3";
-eat.src = "audio/eat.mp3";
-up.src = "audio/up.mp3";
-right.src = "audio/right.mp3";
-left.src = "audio/left.mp3";
-down.src = "audio/down.mp3";
+dead.src = 'audio/dead.mp3';
+eat.src = 'audio/eat.mp3';
+up.src = 'audio/up.mp3';
+right.src = 'audio/right.mp3';
+left.src = 'audio/left.mp3';
+down.src = 'audio/down.mp3';
 
 // Create the snake
-
 let snake = [];
 
 snake[0] = {
@@ -44,7 +39,6 @@ snake[0] = {
 };
 
 // Create the food
-
 let food = {
   // Easy mode not appearing near the walls
   x: Math.floor(Math.random() * 14 + 2) * box,
@@ -61,26 +55,26 @@ let score = 0;
 // Control the snake
 let d;
 
-const startButton = document.getElementById("startButtonid");
+const startButton = document.getElementById('startButtonid');
 function startButtonF() {
-  d = "RIGHT";
+  d = 'RIGHT';
   right.play();
 }
-startButton.addEventListener("click", startButtonF);
+startButton.addEventListener('click', startButtonF);
 
-document.addEventListener("keydown", direction);
+document.addEventListener('keydown', direction);
 function direction(event) {
-  if (event.keyCode == 37 && d !== "RIGHT") {
-    d = "LEFT";
+  if (event.keyCode == 37 && d !== 'RIGHT') {
+    d = 'LEFT';
     left.play();
-  } else if (event.keyCode == 38 && d !== "DOWN") {
-    d = "UP";
+  } else if (event.keyCode == 38 && d !== 'DOWN') {
+    d = 'UP';
     up.play();
-  } else if (event.keyCode == 39 && d !== "LEFT") {
-    d = "RIGHT";
+  } else if (event.keyCode == 39 && d !== 'LEFT') {
+    d = 'RIGHT';
     right.play();
-  } else if (event.keyCode == 40 && d !== "UP") {
-    d = "DOWN";
+  } else if (event.keyCode == 40 && d !== 'UP') {
+    d = 'DOWN';
     down.play();
   }
 }
@@ -102,10 +96,10 @@ function draw() {
   ctx.drawImage(ground, 0, 0);
 
   for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = i === 0 ? "green" : "white";
+    ctx.fillStyle = i === 0 ? 'green' : 'white';
     ctx.fillRect(snake[i].x, snake[i].y, box, box);
 
-    ctx.strokeStyle = "red";
+    ctx.strokeStyle = 'red';
     ctx.strokeRect(snake[i].x, snake[i].y, box, box);
   }
 
@@ -115,15 +109,15 @@ function draw() {
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  ctx.fillStyle = "white";
-  ctx.font = "45px changa one";
+  ctx.fillStyle = 'white';
+  ctx.font = '45px changa one';
   ctx.fillText(score, 2 * box, 1.6 * box);
 
   //which direction
-  if (d == "LEFT") snakeX -= box;
-  if (d == "UP") snakeY -= box;
-  if (d == "RIGHT") snakeX += box;
-  if (d == "DOWN") snakeY += box;
+  if (d == 'LEFT') snakeX -= box;
+  if (d == 'UP') snakeY -= box;
+  if (d == 'RIGHT') snakeX += box;
+  if (d == 'DOWN') snakeY += box;
 
   // If the snake eats the food
   if (snakeX === food.x && snakeY === food.y) {
@@ -157,9 +151,9 @@ function draw() {
     dead.play();
 
     // Game over text
-    ctx.fillStyle = "black";
-    ctx.font = "80px bonn";
-    ctx.fillText("Game Over", 4 * box, 10 * box);
+    ctx.fillStyle = 'black';
+    ctx.font = '80px bonn';
+    ctx.fillText('Game Over', 4 * box, 10 * box);
 
     clearInterval(game);
   }
@@ -168,13 +162,13 @@ function draw() {
 }
 
 // Restart button
-const restartButton = document.getElementById("restartButtonid");
-restartButton.addEventListener("click", restartButtonF);
+const restartButton = document.getElementById('restartButtonid');
+restartButton.addEventListener('click', restartButtonF);
 function restartButtonF() {
   location.reload();
 }
 
-let game = setInterval(draw, 100);
+let game = setInterval(draw, 50);
 
-// ctx.fillStyle = "red";
-// ctx.fillRect(10, 10, 100, 100);
+// ctx.fillStyle = 'red';
+ctx.fillRect(10, 10, 100, 100);
